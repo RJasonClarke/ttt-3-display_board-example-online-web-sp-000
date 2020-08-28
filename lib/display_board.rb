12442d4c39606d8c331df0d4bf8d08c10209b281
Robert Clarke <r.jason.clarke@gmail.com>
2020-08-28 01:22:25 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,7 @@
-row_1 = "   |   |   "
-row_2 = "-----------"
-
-def board_display
-
-  tic_tac = puts row_1
-  puts row_2
-  puts row_1
-  puts row_2
-  puts row_1
-
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
